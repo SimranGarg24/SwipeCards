3d33b847e49fd5ecaef4f31e7ad8b119b91a03b1
simran.garg.chicmic <129032952+SimranGarg24@users.noreply.github.com>
2023-08-07 11:17:27 +0530

    Add files via upload

---
+++ SwipeCardsSDK/SwipeCardsSDK/SwipeCardsSDK.h
@@ -0,0 +1,18 @@
+//
+//  SwipeCardsSDK.h
+//  SwipeCardsSDK
+//
+//  Created by Saheem Hussain on 07/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwipeCardsSDK.
+FOUNDATION_EXPORT double SwipeCardsSDKVersionNumber;
+
+//! Project version string for SwipeCardsSDK.
+FOUNDATION_EXPORT const unsigned char SwipeCardsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeCardsSDK/PublicHeader.h>
+
+
